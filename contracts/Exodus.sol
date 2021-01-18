860484c67788103968d6f94ce68a192cf898af23
hassan-s47 <hhsheikh77@gmail.com>
2021-01-18 16:33:21 +0500

    Smart Contract v2.0

--- contracts/Exodus.sol
+++ contracts/Exodus.sol
@@ -3,21 +3,12 @@ pragma solidity >=0.4.0 <0.8.0;
 contract Exodus{
 
     address public owner;
-    uint16 public transactionCounter = 0;
-
-    struct Transaction{
-        bytes8 txtype;
-        bytes32 date;
-        bytes32 userid;
-        uint amount;
-    }
+    uint16 public userCounter = 0;
 
     struct User{
-        bytes32 id;
-        uint balance;
-        bool subscription;
+        bool subscribe;
+        uint32 balance;
     }
-
 
     constructor() public{
         owner = msg.sender;
@@ -28,74 +19,39 @@ contract Exodus{
         _;
     }
     //Check the subscription of the user.
-    modifier isSubscribed(bytes32 useraddress) {
-        require(users[useraddress].subscription);
+    modifier isSubscribed(bytes16 useraddress) {
+        require(users[useraddress].subscribe);
         _;
     }
-
-    event transactionCompleted(
-        bytes32 txid
-    );
     //Users data is accessible by their addresses
-    mapping (bytes32 => User) users; 
-    mapping (bytes32 => Transaction ) transactions;
+    mapping (bytes16 => User) users; 
 
     //Users subscription is proesssed by the owners only
-    function subscribe(bytes32 id,bytes32 userid,bytes32 date,bytes8 txtype,uint amount) public
+    function subscribe(bytes16 userid) public
     {
-        require(users[userid].subscription == false);
-        users[userid].subscription = true;
-        transactionCounter++;
-        transactions[id] = Transaction(txtype, date, userid, amount);
-        require(users[userid].subscription);
-        emit transactionCompleted(id);
+        require(users[userid].subscribe == false);
+        users[userid].subscribe = true;
+        require(users[userid].subscribe == false);
     }
     
-    function unsubscribe(bytes32 useraddress) public onlyOwner() 
+    function unsubscribe(bytes16 userid) public onlyOwner() isSubscribed(userid)
     {
-        users[useraddress].subscription = false;
-        require(users[useraddress].subscription == false);
+        users[userid].subscribe = true;
+        require(users[userid].subscribe == false);
     }
 
-    function savePayment(bytes32 id,bytes32 userid,bytes32 date,bytes8 txtype,uint amount) public onlyOwner()
+    function increment_balance(bytes16 userid, uint32 amount) public onlyOwner() 
     {
-        require(users[userid].subscription == true);
-        users[userid].subscription = true;
-        transactionCounter++;
-        transactions[id] = Transaction(txtype, date, userid, amount);
-        emit transactionCompleted(id);
-    }
-    
-    function saveWithdrawal(bytes32 id,bytes32 userid,bytes32 date,bytes8 txtype,uint amount) public onlyOwner()
-    {
-        require(users[userid].subscription == true);
-        users[userid].subscription = true;
-        transactionCounter++;
-        uint temp = users[userid].balance;
-        users[userid].balance -= amount;
-        transactions[id] = Transaction(txtype, date, userid, amount);
-        require(users[userid].balance == (temp - amount));
-        emit transactionCompleted(id);
-    }
-    
-    function saveDisbursement(bytes32 id,bytes32 userid,bytes32 date,bytes8 txtype,uint amount) public onlyOwner()
-    {
-        users[userid].subscription = true;
-        transactionCounter++;
-        uint temp = users[userid].balance;
         users[userid].balance += amount;
-        transactions[id] = Transaction(txtype, date, userid, amount);
-        require(users[userid].balance == (temp + amount));
-        emit transactionCompleted(id);
     }
 
-    function getTransaction(bytes32 id) public view onlyOwner() returns (bytes8 ,bytes32 ,bytes32 , uint) 
+    function decrement_balance(bytes16 userid,uint32 amount) public onlyOwner() isSubscribed(userid)
     {
-        return(transactions[id].txtype,transactions[id].userid,transactions[id].date,transactions[id].amount);
+        require(users[userid].balance >= amount);
+        users[userid].balance -= amount;
     }
-    
 
-    function getBalance(bytes32 userid) public  view returns(uint)
+    function getBalance(bytes16 userid) public  view returns(uint)
     {
         return(users[userid].balance);
     }
